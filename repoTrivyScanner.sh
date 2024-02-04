b2af8e387294644a3589d80a5a6cd648707408fa
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2024-02-04 18:13:03 +0530

    Update repoTrivyScanner.sh

--- repoTrivyScanner.sh
+++ repoTrivyScanner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source BP-BASE-SHELL-STEPS/functions.sh
 source BP-BASE-SHELL-STEPS/log-functions.sh
-cd  "$WORKSPACE/$CODEBASE_DIR"
+cd  "${WORKSPACE}"/"${CODEBASE_DIR}"
 
 if [ -d "reports" ]; then
     true
@@ -12,29 +12,29 @@ fi
 STATUS=0
 
     logInfoMessage "I'll scan source-code licenses ${WORKSPACE}/${CODEBASE_DIR} for only ${SCAN_SEVERITY} severities"
-    sleep  $SLEEP_DURATION
+    sleep  "$SLEEP_DURATION"
     logInfoMessage "Executing command"
     logInfoMessage "trivy repo -q --severity ${SCAN_SEVERITY} --scanners ${SCAN_TYPE} --exit-code 1 ${FORMAT_ARG} -o reports/${OUTPUT_ARG} ${WORKSPACE}/${CODEBASE_DIR}"
-    trivy repo -q --severity ${SCAN_SEVERITY} --scanners ${SCAN_TYPE} --exit-code 1 ${FORMAT_ARG} -o reports/${OUTPUT_ARG} ${WORKSPACE}/${CODEBASE_DIR}
-STATUS=`echo $?`
+    trivy repo -q --severity "${SCAN_SEVERITY}" --scanners "${SCAN_TYPE}" --exit-code 1 "${FORMAT_ARG}" -o reports/"${OUTPUT_ARG}" "${WORKSPACE}"/"${CODEBASE_DIR}"
+STATUS=$(echo $?)
 if [ -s "reports/${OUTPUT_ARG}" ]; then
-   cat reports/${OUTPUT_ARG}
+   cat reports/"${OUTPUT_ARG}"
 else
     echo "NO ${SCAN_TYPE} FOUND IN SOURCE CODE "
 fi
 
 
-if [ $STATUS -eq 0 ]
+if [ "$STATUS" -eq 0 ]
 then
   logInfoMessage "Congratulations trivy scan succeeded!!!"
-  generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Congratulations trivy scan succeeded!!!"
-elif [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
+  generateOutput "${ACTIVITY_SUB_TASK_CODE}" true "Congratulations trivy scan succeeded!!!"
+elif [ "$VALIDATION_FAILURE_ACTION" == "FAILURE" ]
   then
     logErrorMessage "Please check triyv scan failed!!!"
-    generateOutput ${ACTIVITY_SUB_TASK_CODE} false "Please check triyv scan failed!!!"
+    generateOutput "${ACTIVITY_SUB_TASK_CODE}" false "Please check triyv scan failed!!!"
     exit 1
    else
     logWarningMessage "Please check triyv scan failed!!!"
-    generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Please check triyv scan failed!!!"
+    generateOutput "${ACTIVITY_SUB_TASK_CODE}" true "Please check triyv scan failed!!!"
     exit 1
 fi
